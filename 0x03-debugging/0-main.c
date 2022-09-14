70246d5f8f7afab0de7ad3c99b031a53a70ed143
Gee3eeo <isanborgerald@gmail.com>
2022-09-14 16:56:22 -0700

    Positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,15 @@
 
 /**
   * main - Test positive or negative
-  * Return: Always 0 
+  * Return: 0 
   */
 
 int main(void)
 {
-int i;
-i = 0;
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
 
-positive_or_negative(i);
-
-return (0);
+	return (0);
 }
