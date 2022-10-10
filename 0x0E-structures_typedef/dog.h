a8af4655da28e50b494841aecd8261d757cb7597
Gee3eeo <isanborgerald@gmail.com>
2022-10-10 14:37:01 -0700

    Dog lover, free dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,6 +6,7 @@
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
+ * Description: This struct is for dog
  */
 
 struct dog
@@ -13,19 +14,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-
-/**
- * dog_t - Typedef for dog
- */
+}
 
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
